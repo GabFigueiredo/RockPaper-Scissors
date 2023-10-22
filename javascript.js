e300bbadf2da7c94f0a5c6cd39a1c7bfa057c073
rayobiel <gabrielfandradee2@gmail.com>
2023-10-21 21:06:19 -0300

    Done! Took lengthy.

--- javascript.js
+++ javascript.js
@@ -1,22 +1,69 @@
-let user_choice
-let computer_choice
+let user_choice = 0
+let computer_choice = 0
+let ComputerScore = 0;
+let UserScore = 0;
 
 function playRound() {
-    let ComputerScore
-    let UserScore
-    for (let i = 0; i <= 5; i++) {
-    user_choice = getUserChoice();
-    computer_choice = getComputerChoice();
-    let result = game()
-    
+    for (let i = 1; i <= 5; i++) {
+        user_choice = getUserChoice();
+        computer_choice = getComputerChoice();
+        let result = winConditions();
     if (result.includes('win')) {
-        UserScore++
-        return 'Round ${i}: You win!'+{user_choice}+'beats'+{computer_choice};} 
-    else if (result.includes('lose')) {
-        ComputerScore++
-        return `Round ${i}: You lose! ${computer_choice}+ beats +${user_choice}`}
-        }
-    
+        UserScore++;}
+    else if (result.includes('lost')) {
+        ComputerScore++;
+    }
+
+    console.log(`Round ${i}: Computer chose ${computer_choice} it is a ${result}`)   
+  }
+    if (ComputerScore > UserScore) {
+        return 'Computer WON'
+    }
+    else if (UserScore > ComputerScore) {
+        return 'You WON'
+    }
+    else if (UserScore === ComputerScore) {
+        return 'it is a rare draw'
+    } 
 }
 
-let test = `Round ${i}: You lose! ${computer_choice}+ beats +${user_choice}`
+function getUserChoice() {
+    user_choice = prompt('Choose Rock, Paper or Scissor');
+    user_choice = user_choice.toLowerCase();    
+    return user_choice;
+}
+
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    let res;
+    if (randomNumber === 0) {
+        res = 'rock'
+    }
+    else if(randomNumber === 1) {
+        res = 'paper'
+    }
+    else if(randomNumber === 2) {
+        res = 'scissors'
+    }
+
+    return res 
+}
+
+function winConditions() {
+    switch (user_choice+computer_choice) {
+        case 'paperrock':
+        case 'rockscissors':
+        case 'scissorspaper':
+        return 'win'
+        break;
+        case 'rockpaper':
+        case 'scissorsrock':
+        case 'paperscissors':
+        return 'lost'
+        break;
+        case 'rockrock':
+        case 'paperpaper':
+        case 'scissorsscissors':
+        return 'draw';    
+    }
+}
